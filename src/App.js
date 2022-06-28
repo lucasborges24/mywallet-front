45d9a5bb7b037b841dd4ecd420fd3ec7bd009996
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-06-28 11:47:29 -0300

    feat: add routes to with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import { BrowserRouter, Routes, Route, Switch } from 'react-router-dom';
 import { useState } from 'react';
 
+import Cadastro from './cadastro/Cadastro';
+import Editar from './editar/Editar';
+import Entrada from './entrada/Entrada';
+import Home from './home/Home';
+import Login from './home/Home';
+import Saida from './saida/Saida';
+
 
 function App() {
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/cadastro' element={<Cadastro />} />
+        <Route path='/editar' element={<Editar />} />
+        <Route path='/entrada' element={<Entrada />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/saida' element={<Saida />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
